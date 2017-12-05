03ef6280fc025f460461a5c4fe4acd7746e9bb5e
sColin16 <csil167593@adams12.org>
2017-12-05 16:53:07 -0700

    fix whitespace

--- zerver/tests/test_messages.py
+++ zerver/tests/test_messages.py
@@ -2748,10 +2748,8 @@ class MessageHydrationTest(ZulipTestCase):
 
         self.assertIn('class="user-mention"', new_message['content'])
         self.assertEqual(new_message['flags'], ['mentioned'])
-        
-        
+
 class OctopusTest(ZulipTestCase):
-    
     def test_change_welcome_message(self) -> None:
         sender = get_user('iago@zulip.com', get_realm('zulip'))
         client = make_client(name="test suite")
